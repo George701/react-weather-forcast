fc7e009e5303ae03e47f30d1ac34a12bfad5b2b4
George701 <chernikov.georgii@gmail.com>
2019-04-04 21:48:55 +0200

    Updates

---
+++ precache-manifest.cd71997f94e9829c7ae30c31b879e6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "866cb20bf7ec86af4fc0f838a4a50bef",
+    "url": "/react-weather-forcast/static/media/loader.866cb20b.gif"
+  },
+  {
+    "revision": "7c6d36aee02d84ecd257",
+    "url": "/react-weather-forcast/static/js/runtime~main.2634a607.js"
+  },
+  {
+    "revision": "583c8229c60f6400c295",
+    "url": "/react-weather-forcast/static/js/main.3a2f0387.chunk.js"
+  },
+  {
+    "revision": "6b8cb087309717a76d95",
+    "url": "/react-weather-forcast/static/js/2.f1c6b5dc.chunk.js"
+  },
+  {
+    "revision": "583c8229c60f6400c295",
+    "url": "/react-weather-forcast/static/css/main.f089df83.chunk.css"
+  },
+  {
+    "revision": "26170303a9fe9be5a052653a46ae450f",
+    "url": "/react-weather-forcast/index.html"
+  }
+];
