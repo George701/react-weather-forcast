e5c5a892a6b60c9087fe97b97a7c0c26b707c048
George701 <chernikov.georgii@gmail.com>
2019-04-05 12:30:27 +0300

    Updates

--- precache-manifest.1625dfeba41461634e87f62ccb743cce.js
+++ precache-manifest.1625dfeba41461634e87f62ccb743cce.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/react-weather-forcast/static/js/runtime~main.2634a607.js"
   },
   {
-    "revision": "8f1252c9cf2ca9107385",
-    "url": "/react-weather-forcast/static/js/main.3e93ecc6.chunk.js"
+    "revision": "786c494336009b1fe870",
+    "url": "/react-weather-forcast/static/js/main.98da8ccf.chunk.js"
   },
   {
     "revision": "6b8cb087309717a76d95",
     "url": "/react-weather-forcast/static/js/2.f1c6b5dc.chunk.js"
   },
   {
-    "revision": "8f1252c9cf2ca9107385",
+    "revision": "786c494336009b1fe870",
     "url": "/react-weather-forcast/static/css/main.d4f3992b.chunk.css"
   },
   {
-    "revision": "3b63999253b7d25f9832218eba35ff9b",
+    "revision": "1c18d6335e966c568eb25a2bd6d8edf7",
     "url": "/react-weather-forcast/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-weather-forcast/precache-manifest.c82de8e3fffde828e3620244f9a37f70.js"
+  "/react-weather-forcast/precache-manifest.1625dfeba41461634e87f62ccb743cce.js"
 );
 
 workbox.clientsClaim();
